0b1f683e42ea8972be1b794637696d36af5d0b7d
Duy Anh <duyanhdao297@gmail.com>
2025-06-30 23:42:10 +0700

    feat(api): add post route to create a tour

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -3,17 +3,19 @@ const express = require('express');
 
 const app = express();
 
-// app.get('/', (req, res) => {
-//    res.status(200).json({
-//       message: 'Hello from the server side!',
-//       app: 'Natours',
-//    });
-// });
+app.use(express.json());
 
-// app.post('/', (req, res) => {
-//    res.send('You can post to this endpoint');
-// });
-
+/*
+app.get('/', (req, res) => {
+   res.status(200).json({
+      message: 'Hello from the server side!',
+      app: 'Natours',
+   });
+});
+app.post('/', (req, res) => {
+   res.send('You can post to this endpoint');
+});
+*/
 const tours = JSON.parse(
    fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
@@ -26,6 +28,42 @@ app.get('/api/v1/tours', (req, res) => {
          tours,
       },
    });
+});
+
+// req.params: object that store all the parameters from the url
+// Add ? for optinal parameters
+app.get('/api/v1/tours/:id', (req, res) => {
+   console.log(req.params);
+   const tour = tours.find((el) => el.id === req.params);
+   res.status(200).json({
+      status: 'success',
+      // results: tours.length,
+      // data: {
+      //    tours,
+      // },
+   });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+   // console.log(req.body); // If you dont use middleware, express doesn't read the body data, so log for req.body will be undefined
+
+   const newId = tours[tours.length - 1].id + 1;
+   const newTour = Object.assign({ id: newId }, req.body);
+
+   tours.push(newTour);
+
+   fs.writeFile(
+      `${__dirname}/dev-data/data/tours-simple.json`,
+      JSON.stringify(tours),
+      (err) => {
+         res.status(201).json({
+            status: 'success',
+            data: {
+               tour: newTour,
+            },
+         });
+      }
+   );
 });
 
 const port = 3000;
