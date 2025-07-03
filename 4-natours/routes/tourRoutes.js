815ff1825d7d5f605428a76eb940b166bdaedc77
Duy Anh <duyanhdao297@gmail.com>
2025-07-03 15:07:26 +0700

    feat(api): add checkbody chaining middleware

--- 4-natours/routes/tourRoutes.js
+++ 4-natours/routes/tourRoutes.js
@@ -6,12 +6,18 @@ const {
    updateTour,
    deleteTour,
    checkId,
+   checkBody,
 } = require('./../controllers/tourController');
 const router = express.Router();
 
+// Create a checkBody middleware
+// Check if body contains the name and price property
+// If not, send back 400
+// Add it to the post
+
 router.param('id', checkId);
 
-router.route('/').get(getAllTours).post(createTour);
+router.route('/').get(getAllTours).post(checkBody, createTour);
 
 router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
 
