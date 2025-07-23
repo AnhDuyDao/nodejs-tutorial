ff66ff841368c622455969b34abfe5eac4a87033
Duy Anh <duyanhdao297@gmail.com>
2025-07-24 00:22:13 +0700

    feat(api): global operational erros middleware

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -17,10 +17,10 @@ app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
 // Custom middleware
-app.use((req, res, next) => {
-   console.log('Hello from the middleware');
-   next(); // IMPORTANT
-});
+// app.use((req, res, next) => {
+//    console.log('Hello from the middleware');
+//    next(); // IMPORTANT
+// });
 
 // Custom middleware to set request time
 app.use((req, res, next) => {
@@ -32,5 +32,31 @@ app.use((req, res, next) => {
 // Mount router
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+// 3) ERROR HANDLING
+// '*' wildcard means all routes that are not defined above
+// This should be the last middleware
+// If it not, it will catch all routes and return a 404 error, undefined route
+app.all('*', (req, res, next) => {
+   // res.status(404).json({
+   //    status: 'fail',
+   //    message: `Can't find ${req.originalUrl} on this server!`
+   // });
+   const err = new Error(`Can't find ${req.originalUrl} on this server!`);
+   err.statusCode = 404;
+   err.status = 'fail';
+   next(err);
+});
+
+// Write a handler operational errors
+app.use((err, req, res, next) => {
+   err.statusCode = err.statusCode || 500;
+   err.status = err.status || 'error';
+
+   res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message
+   });
+});
 
 module.exports = app;
